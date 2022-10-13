e3fc4e2722f6b1fb250de769dd32d2b75403325f
Fabio Kruchielski <fabio@devnx.com.br>
2022-10-13 12:08:53 -0300

    deixar o token fixo

--- backend/src/database/seeds/20200904070006-create-apiToken-settings.ts
+++ backend/src/database/seeds/20200904070006-create-apiToken-settings.ts
@@ -8,7 +8,8 @@ module.exports = {
       [
         {
           key: "userApiToken",
-          value: uuidv4(),
+          value: '56e428d6-8757-421b-8a5c-3484a5eb667d',
+          // value: uuidv4(),
           createdAt: new Date(),
           updatedAt: new Date()
         }
